28d4bcf524ebd030cc1b5c846dba23ac34e0c19f
Larissa Kelly <larissak0@hotmail.com>
2023-08-13 13:25:36 -0300

    :tada: initial commit

---
+++ assets/script.js
@@ -0,0 +1,30 @@
+function toggleMode() {
+  const html = document.documentElement
+  html.classList.toggle("light")
+
+  // pegar img //
+
+  const img = document.querySelector("#profile img")
+
+  // substituir a imagem //
+
+  if (html.classList.contains("light")) {
+    // se tiver light mode, adicionar a imagem light
+    img.setAttribute("src", "./assets/assets/larisglasses.png")
+  }
+
+  else {
+    // se tiver sem light mode, manter a imagem normal
+    img.setAttribute("scr", "./assets/assets/larisprofile.png")
+  }
+
+
+/* if(html.classList.contains('light')){
+  html.classList.remove('light')
+}
+  else {
+    html.classList.add('light')
+  }*/
+
+
+}
